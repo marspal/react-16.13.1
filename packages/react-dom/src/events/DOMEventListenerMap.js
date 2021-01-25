77d210592ac09adc742cbcfb48be9a85ddb48acc
andyxu <xumingquan01@baidu.com>
2021-01-25 21:08:35 +0800

    first commit

---
+++ packages/react-dom/src/events/DOMEventListenerMap.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import type {DOMTopLevelEventType} from 'legacy-events/TopLevelEventTypes';
+
+const PossiblyWeakMap = typeof WeakMap === 'function' ? WeakMap : Map;
+// prettier-ignore
+const elementListenerMap: new PossiblyWeakMap();
+
+export function getListenerMapForElement(
+  element
+) {
+  let listenerMap = elementListenerMap.get(element);
+  if (listenerMap === undefined) {
+    listenerMap = new Map();
+    elementListenerMap.set(element, listenerMap);
+  }
+  return listenerMap;
+}
